b41a771e97c0e6a575c6b48b70393309cc6cfa1b
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-04-18 16:33:28 +0900

    picker view

--- PickerView02/ViewController.swift
+++ PickerView02/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    var color = ["red", "blue", "yellow"]
+    var animal = ["pig", "mouse", "tiger", "dog", "cow"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
+        myLabel.text = "red"
     }
-
-
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    
+        return 2
+    }
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+            if component == 0{
+                return color.count
+            } else {
+                return animal.count
+            }
+        }
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        if component == 0 {
+            return color[row]
+        } else {
+           return animal[row]
+        }
+        
+    }
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        if component == 0{
+        print("row = \(row)")
+        myLabel.text = color[row]
+        } else {
+        myLabel.text = animal[row]
+        }
+    }
+    //public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat{
+        //return 100.0
+    //}
 }
+
 
